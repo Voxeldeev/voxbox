bd096265ac7501388bd6d2f35f1cdcb36ec42d85
Maddy <22009843+AurySystem@users.noreply.github.com>
2021-06-17 02:01:14 -0400

    more icon and stuff tweaks

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -25,7 +25,7 @@ export function prettyNumber(value: number): string {
 
 export class EditorConfig {
 	public static readonly version: string = "2.3"; // Not using patch versions in display right now, maybe someday.
-	public static readonly versionDisplayName: string = "JummBox " + EditorConfig.version;
+	public static readonly versionDisplayName: string = "GoldBox " + EditorConfig.version;
 
 	public static readonly presetCategories: DictionaryArray<PresetCategory> = toNameMap([
 		{
