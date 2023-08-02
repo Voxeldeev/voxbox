16137eabe5cdc6d82a99d3d8b7a48bc9a02871ed
ultraabox <137341681+ultraabox@users.noreply.github.com>
2023-08-02 12:12:15 -0400

    Update AddExternalPrompt.ts

--- editor/AddExternalPrompt.ts
+++ editor/AddExternalPrompt.ts
@@ -1,3 +1,4 @@
+import { Config } from "../synth/SynthConfig";
 import { SongDocument } from "./SongDocument";
 import { HTML, SVG } from "imperative-html/dist/esm/elements-strict";
 import { ColorConfig } from "./ColorConfig";
@@ -6,17 +7,17 @@ import { ColorConfig } from "./ColorConfig";
 export class AddExternalPrompt {
         constructor(_doc) {
             this._doc = _doc;
-            //this.externalSamples = input$6({ style: "width: 18em; margin:5px margin-left: 0px;", type: "text"});
+            //this.externalSamples = input({ style: "width: 18em; margin:5px margin-left: 0px;", type: "text"});
 			//this.externalSamples = HTML.textarea({ rows:"4", cols:"32", type: "text", resize: none, placeholder: "Comment text."});
 			this.externalSamples = HTML.textarea({ style: "resize: none; word-break: break-all;", rows:"8", cols:"32", type: "text", placeholder: "https://example.com/file1.wav!48|https://example.com/file2.ogg,12430.8|https://example.com/file3.mp3@"});
-			this._cancelButton = button$7({ class: "cancelButton" });
-            this._okayButton = button$7({ class: "okayButton", style: "width:45%; display:block" }, "Okay");
-            this.container = div$7({ class: "prompt noSelection", style: "padding-left:25px; width: 250px; display:inline-block; text-align:left" }, h2$6({ style: "text-align:left; margin:0px; padding:0px"} , "Add Samples"), div$7({ style: "display: inline-block; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;" }, div$7({ style: "text-align: left" },
-			p$4("Enter the full url of the sample you'd like to use. Add \" | \" (no spaces) to the very end of a sample url to separate samples. There should be NO SPACES anywhere within this prompt"),
-			p$4("You can set a root key of a sample by adding an exclamation mark followed by the pitch represented in numerical form (ex. C4 is 60) to the end of a sample url. By default, samples are tuned to C4."),
-			p$4("You may  also specify the sample rate by adding a comma followed by the sample rate to the end of a sample url."),
+			this._cancelButton = button({ class: "cancelButton" });
+            this._okayButton = button({ class: "okayButton", style: "width:45%; display:block" }, "Okay");
+            this.container = div({ class: "prompt noSelection", style: "padding-left:25px; width: 250px; display:inline-block; text-align:left" }, h2({ style: "text-align:left; margin:0px; padding:0px"} , "Add Samples"), div({ style: "display: inline-block; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;" }, div({ style: "text-align: left" },
+			p("Enter the full url of the sample you'd like to use. Add \" | \" (no spaces) to the very end of a sample url to separate samples. There should be NO SPACES anywhere within this prompt"),
+			p("You can set a root key of a sample by adding an exclamation mark followed by the pitch represented in numerical form (ex. C4 is 60) to the end of a sample url. By default, samples are tuned to C4."),
+			p("You may  also specify the sample rate by adding a comma followed by the sample rate to the end of a sample url."),
 			"Additionally, you can designate a sample as percussion (doesn't change pitch with key) by adding \"@\" to the end of the sample url:",
-			br$3(), this.externalSamples),div$7({ style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;" }, this._cancelButton)));
+			br(), this.externalSamples),div({ style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;" }, this._cancelButton)));
          
             this.cleanUp = () => {
                 this._okayButton.removeEventListener("click", this._saveChanges);
