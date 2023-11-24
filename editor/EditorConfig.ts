d57b4b8b7219debf5cb910998136e1f7e004010e
MainCharacteroftheProblem <121472288+MainCharacteroftheProblem@users.noreply.github.com>
2023-11-23 20:27:49 -0500

    changes for 2.1.2

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -23,7 +23,7 @@ export function prettyNumber(value: number): string {
 }
 
 export class EditorConfig {
-    public static readonly version: string = "2.1.1"; // Using patch versions in display right now, maybe TODAY.
+    public static readonly version: string = "2.1.2"; // Using patch versions in display right now, maybe TODAY.
     public static readonly versionDisplayName: string = "UltraBox " + EditorConfig.version;
 
     public static readonly releaseNotesURL: string = "./patch_notes.html";
