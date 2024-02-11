e711317f2ffe63b95fe5a06deaa5160a28455c74
choptop84 <88958828+choptop84@users.noreply.github.com>
2024-02-11 13:59:43 -0500

    Update LayoutPrompt.ts

--- editor/LayoutPrompt.ts
+++ editor/LayoutPrompt.ts
@@ -89,7 +89,7 @@ export class LayoutPrompt implements Prompt {
 				div("Flipped Long (AB)"),
 			),
 			label({class: "layout-option"},
-				input({type: "radio", name: "layout", value: "focus"}),
+				input({type: "radio", name: "layout", value: "focused long"}),
 				SVG(`\
 					<svg viewBox="-1 -1 28 22">
 						<rect x="0" y="0" width="26" height="20" fill="none" stroke="currentColor" stroke-width="1"/>
@@ -98,7 +98,7 @@ export class LayoutPrompt implements Prompt {
 						<rect x="2" y="13" width="17" height="5" fill="currentColor"/>
 					</svg>
 				`),
-				div("Focus (AB)"),
+				div("Focused long (AB)"),
 			),
 			// label({class: "layout-option"},
 			// 	input({type: "radio", name: "layout", value: "focused long"}),
