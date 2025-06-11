8570b4401b0fda890f65d74ebf42b55b9efa6fbf
slarmoo <matthew.s.medford@gmail.com>
2025-06-11 18:58:36 -0400

    offline compilation fixed

--- scripts/compile_beepbox_offline.sh
+++ scripts/compile_beepbox_offline.sh
@@ -28,8 +28,8 @@ npx terser \
 npx tsc -p tsconfig_player.json
 
 # Combine build/player/main.js and dependencies into to_deploy/player/beepbox_player.min..js
-npx rollup build/player/main.js \
-	--file to_deploy/player/beepbox_player.min.js \
+npx rollup ./build/player/main.js \
+	--file ./to_deploy/player/beepbox_player.min.js \
 	--format iife \
 	--output.name beepbox \
 	--context exports \
@@ -37,21 +37,21 @@ npx rollup build/player/main.js \
 
 # Minify to_deploy/player/beepbox_player.min.js into to_deploy/player/beepbox_player.min.js
 npx terser \
-	to_deploy/player/beepbox_player.min.js \
-	-o to_deploy/player/beepbox_player.min.js \
+	./to_deploy/player/beepbox_player.min.js \
+	-o ./to_deploy/player/beepbox_player.min.js \
 	--compress \
     --define OFFLINE=true \
 	--mangle \
 	--mangle-props regex="/^_.+/;"
 
-cp website/offline/icon.ico to_deploy/
-cp website/offline/icon.png to_deploy/
-cp website/offline/icon.ico to_deploy/
-cp website/offline/main.js to_deploy/
-cp website/offline/preload.js to_deploy/
-cp website/offline/3JnySDDxiSz36j6yGQ.woff2 to_deploy/
-cp website/offline/jquery-3.4.1.min.js to_deploy/
-cp website/offline/select2.min.css to_deploy/
-cp website/offline/select2.min.js to_deploy/
-cp website/offline/index.html to_deploy/
-cp package.json to_deploy/
+cp ./website/offline/icon.ico to_deploy/
+cp ./website/offline/icon.png to_deploy/
+cp ./website/offline/icon.ico to_deploy/
+cp ./website/offline/main.js to_deploy/
+cp ./website/offline/preload.js to_deploy/
+cp ./website/offline/3JnySDDxiSz36j6yGQ.woff2 to_deploy/
+cp ./website/offline/jquery-3.4.1.min.js to_deploy/
+cp ./website/offline/select2.min.css to_deploy/
+cp ./website/offline/select2.min.js to_deploy/
+cp ./website/offline/index.html to_deploy/
+cp ./package.json to_deploy/
